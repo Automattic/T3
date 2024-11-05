4d97061e489b32ea3421fe6562ceb36fff913946
TommusRhodus <tom.rhodes@automattic.com>
2024-11-05 15:38:23 +0000

    Adds suggestions and default content to post format selector

--- assets/js/build/editor.asset.php
+++ assets/js/build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-editor', 'wp-i18n', 'wp-plugins'), 'version' => '3677323cd8f975fcf07f');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-i18n', 'wp-plugins'), 'version' => '9e879d58692168dd82a6');
--- includes/tumblr-theme-language/lang.php
+++ includes/tumblr-theme-language/lang.php
@@ -568,6 +568,6 @@ return array(
 	'lang:This minimalist Tumblr has no posts'             => __( 'This minimalist Tumblr has no posts.', 'tumblr3' ),
 	'lang:Meditate for a while on this empty Tumblr'       => __( 'Meditate for a while on this empty Tumblr.', 'tumblr3' ),
 	'lang:Posts Nah'                                       => __( 'Posts? Nah.', 'tumblr3' ),
-	'lang:This Tumblr has hardly any posts None at all in fact' => __( 'This Tumblr has hardly any posts. \nNone at all, in fact.', 'tumblr3' ),
+	'lang:This Tumblr has hardly any posts None at all in fact' => __( 'This Tumblr has hardly any posts. None at all, in fact.', 'tumblr3' ),
 	'lang:This Tumblr hasnt posted anything'               => __( 'This Tumblr hasn\'t posted anything.', 'tumblr3' ),
 );
