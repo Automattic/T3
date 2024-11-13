7ca2928f90220b5d9e086685c3cfa46191f3672e
Rafael Meneses <meneses.tio@gmail.com>
2024-11-13 17:16:37 -0300

    other stuff

--- assets/js/build/editor.asset.php
+++ assets/js/build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '8a2ce0508e797b60b350');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => 'bb58f17b4511295bda87');
