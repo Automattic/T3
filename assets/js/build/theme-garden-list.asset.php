409cd89edd51a0cda038d47634c54a8f61d0951d
roccotrip <rocco.tripaldi@automattic.com>
2024-11-21 23:51:02 -0500

    wtf

--- assets/js/build/theme-garden-list.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5bfc2da2ef125d567436');
--- assets/js/build/theme-garden-store.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-data'), 'version' => '3b9e861a294651cc0e56');
