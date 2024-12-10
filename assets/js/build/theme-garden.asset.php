98907f9aae758110145fe53b7b5b4bdd023fc356
Rafael Meneses <meneses.tio@gmail.com>
2024-12-10 17:30:37 -0300

    increase waiting time before searching

--- assets/js/build/theme-garden.asset.php
+++ assets/js/build/theme-garden.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9727829e65ee1958b005');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'bca198c2edb596cf77e9');
