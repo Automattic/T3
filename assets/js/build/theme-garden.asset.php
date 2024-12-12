16454e0bcdc0e2056904e93ed4898b23f24f0710
Rafael Meneses <meneses.tio@gmail.com>
2024-12-12 09:47:49 -0300

    Increase the wait time before searching (#60)
    * increase waiting time before searching
    
    * decrease wait time to 800

--- assets/js/build/theme-garden.asset.php
+++ assets/js/build/theme-garden.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '23cb00bfe1a13bb91f42');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c7e8562827badd92583f');
